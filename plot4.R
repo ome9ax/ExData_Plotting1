ab90090dc0ba3bf0e91b2ef302df0f280c93a68d
Eddy Delta <edrdelta@gmail.com>
2021-01-28 11:42:16 +0000

    Peer-graded Assignment: Course Project 1 - update

--- plot4.R
+++ plot4.R
@@ -5,25 +5,28 @@ if(!file.exists(zip_file))
     download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', zip_file)
 
 # Read the data file and filter the dates
-data <- subset(read.table(unz(zip_file, paste0(file_name, '.txt')), sep = ';', header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE, na.strings = '?'), Date %in% c('1/2/2007', '2/2/2007'))
-data$Time <- strptime(paste0(data$Date, data$Time), format = '%d/%m/%Y%H:%M:%S') # convert into datetime
+data <- subset(read.table(unz(zip_file, paste0(file_name, '.txt')), sep = ';', header = TRUE, stringsAsFactors = FALSE, strip.white = TRUE, na.strings = c('?', 'NA', '')), Date %in% c('1/2/2007', '2/2/2007'))
+data$Time <- strptime(paste(data$Date, data$Time), format = '%d/%m/%Y %H:%M:%S') # convert into datetime
 data$Date <- as.Date(data$Date, format = '%d/%m/%Y') # convert into date
 data[, 3:9] <- apply(data[, 3:9], 2, function(x) as.numeric(x)) # convert into numeric
 
 old.par <- par() # mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1), oma = c(0, 0, 0, 0)
-png('plot4.png', width = 480, height = 480)
+png('plot4.png', width = 480, height = 480, units = 'px')
 par(mfcol = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 0, 0))
 
-with(data, plot(Time, Global_active_power, type = 'l', ylab = 'Global Active Power', xlab = ''))
+with(data, {
+    plot(Time, Global_active_power, type = 'l', ylab = 'Global Active Power', xlab = '')
 
-with(data, plot(Time, Sub_metering_1, type = 'l', ylab = 'Energy sub metering', xlab = ''))
-with(data, points(Time, Sub_metering_2, type = 'l', col = 'red'))
-with(data, points(Time, Sub_metering_3, type = 'l', col = 'blue'))
-legend('topright', lwd = rep(2.5, 3), col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), bty = 'n')
+    plot(Time, Sub_metering_1, type = 'l', ylab = 'Energy sub metering', xlab = '')
+    lines(Time, Sub_metering_2, col = 'red')
+    lines(Time, Sub_metering_3, col = 'blue')
+    legend('topright', lwd = 2.5, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), bty = 'n')
 
-with(data, plot(Time, Voltage, type = 'l', ylab = 'Voltage', xlab = 'datetime'))
+    plot(Time, Voltage, type = 'l', ylab = 'Voltage', xlab = 'datetime')
 
-with(data, plot(Time, Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = 'datetime'))
+    plot(Time, Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = 'datetime')
+})
+
 dev.off()
 par(old.par)
 
